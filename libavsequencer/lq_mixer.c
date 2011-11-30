366787943097923c48152c8684dfefd579e9852b
Sebastian Vater <cdgs.basty@googlemail.com>
2011-11-30 10:57:47 +0100

    Fixed one unnecessary parenthesis in low quality mixer.

--- libavsequencer/lq_mixer.c
+++ libavsequencer/lq_mixer.c
@@ -3782,7 +3782,7 @@ static av_cold uint32_t set_volume(AVMixerData *mixer_data, uint32_t amplify, ui
     AV_LQMixerChannelInfo *const old_channel_info = lq_mixer_data->channel_info;
     uint32_t old_channels, i;
 
-    if (((old_channels = lq_mixer_data->channels_in) != channels) && (!(channel_info = av_mallocz((channels * sizeof(AV_LQMixerChannelInfo)) + FF_INPUT_BUFFER_PADDING_SIZE)))) {
+    if (((old_channels = lq_mixer_data->channels_in) != channels) && !(channel_info = av_mallocz((channels * sizeof(AV_LQMixerChannelInfo)) + FF_INPUT_BUFFER_PADDING_SIZE))) {
         av_log(lq_mixer_data->mixer_data.mixctx, AV_LOG_ERROR, "Cannot allocate mixer channel data.\n");
 
         return old_channels;
